6d263c320da7f9ab62b3a2d93608252ae1644bca
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-29 12:32:01 +0200

    ok

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+  version         = ">= 2.0"
+  environment     = "public"
+ # subscription_id = var.azure_subscription_id
+ # client_id       = var.azure_client_id
+ # client_secret   = var.azure_client_secret
+ # tenant_id       = var.azure_tenant_id
 }
 
 resource "random_id" "ip_dns" {
