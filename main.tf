656249009fd27fb3d61e26637cb419bf448d6c3a
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-29 10:32:25 +0200

    Step 2 — Creating Modules

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 #create resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-MyFirstTerraform"
+    name     = "rg-TerraformDemo"
     location = "north europe"
 }
 
