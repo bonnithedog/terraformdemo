e13aae8dd57bcf222c6f11563629aa064818ae5c
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-08-01 00:11:22 +0200

    nsgvnet

--- main.tf
+++ main.tf
@@ -14,9 +14,18 @@ module "resource_group" {
 #    location  = var.location
 #}
 
-#create azurerm_network_security_group group
-module "network_security_group" {
+
+}
+
+
+module "vnet" {
     source    = "./modules/vnet"
+    address_space       = ["10.0.0.0/16"]
+    subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    subnet_names        = ["subnet1", "subnet2", "subnet3"]
     rgname    = var.envset
     location  = var.location
-}
+
+  nsg_ids = {
+    subnet1 = azurerm_network_security_group.nsg1.id
+  }
