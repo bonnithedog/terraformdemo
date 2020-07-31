e432e3391c8cae11c087ec96d6c130ed513e5e3b
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-31 22:08:29 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,10 @@ variable "envset" {
     description = "Name environment settings for demo test or prod"
     default = "Demo"
 }
+
+
+variable "location" {
+    type = string
+    description = "Name environment settings for demo test or prod"
+    default = "northeurope"
+}
