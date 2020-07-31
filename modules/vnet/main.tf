ec95fd332487b9e3ff6bda2ab4d4405ff69d5b67
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-08-01 00:16:20 +0200

    ok

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -10,17 +10,17 @@ module "vnet" {
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-  #nsg_ids = {
-  #  subnet1 = azurerm_network_security_group.ssh.id
-  #  subnet2 = azurerm_network_security_group.ssh.id
-  #  subnet3 = azurerm_network_security_group.ssh.id
-  #}
-#
-#
-  #tags = {
-  #  environment = "dev"
-  #  costcenter  = "it"
-  #}
+  nsg_ids = {
+    subnet1 = azurerm_network_security_group.ssh.id
+    subnet2 = azurerm_network_security_group.ssh.id
+    subnet3 = azurerm_network_security_group.ssh.id
+  }
+
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 }
 
 resource "azurerm_network_security_group" "ssh" {
