3ac3939761c69822ccc3193c6d65ee416a8f1114
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-31 21:41:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 #create resource group
 module "resource_group" "rg" {
     source    = "./modules/rg"
-    name    = "demo"
-    location = "westus"
+    #name    = "demo"
+    #location = "westus"
 }
 
