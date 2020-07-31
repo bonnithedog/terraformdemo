d8143e3038cc5cd4335658c00b081ae8fbec3415
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-08-01 00:33:24 +0200

    ##

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ module "resource_group" {
     location  = var.location
 }
 
-#create azurerm_network_security_group group
-module "network_security_group" {
-    source    = "./modules/nsg"
-    rgname    = var.envset
-    location  = var.location
-}
+##create azurerm_network_security_group group
+#module "network_security_group" {
+#    source    = "./modules/nsg"
+#    rgname    = var.envset
+#    location  = var.location
+#}
 
 
 
