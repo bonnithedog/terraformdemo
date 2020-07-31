6a0eda463bb4ad67191e777e84d72daf15761d67
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-08-01 00:37:57 +0200

    333

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ module "resource_group" {
 
 module "vnet" {
     source    = "./modules/vnet"
-    address_space       = ["10.0.0.0/16"]
-    subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    subnet_names        = ["subnet1", "subnet2", "subnet3"]
+    #address_space       = ["10.0.0.0/16"]
+    #subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    #subnet_names        = ["subnet1", "subnet2", "subnet3"]
     rgname    = var.envset
     location  = var.location
 
-  nsg_ids = {
-    subnet1 = azurerm_network_security_group.nsg1.id
+  #nsg_ids = {
+  # subnet1 = azurerm_network_security_group.nsg1.id
   }
 
   }
