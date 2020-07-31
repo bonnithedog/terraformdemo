b6846005e44f4313efbeb685bd92e16b63cd697b
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-31 17:17:31 +0200

    ok

--- main.tf
+++ main.tf
@@ -2,31 +2,31 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_id" "rg_name" {
+resource "random_id" "test_name" {
   byte_length = 8
 }
 
 resource "azurerm_resource_group" "test" {
-  name     = "test-${random_id.rg_name.hex}-rg"
+  name     = "test-${random_id.test_name.hex}-test"
   location = var.location
 }
 
 
 resource "azurerm_network_security_group" "nsg1" {
-  name                = "test-${random_id.rg_name.hex}-nsg"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                = "test-${random_id.test_name.hex}-nsg"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
 }
 
 resource "azurerm_route_table" "rt1" {
-  location            = azurerm_resource_group.rg.location
-  name                = "test-${random_id.rg_name.hex}-rt"
+  location            = azurerm_resource_group.test.location
+  name                = "test-${random_id.test_name.hex}-rt"
   resource_group_name = azurerm_resource_group.test.name
 }
 
 module "vnet" {
   source              = "./modules/vnet/"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.test.name
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
