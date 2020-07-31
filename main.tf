b314c06ef489d4d074733b13e633821b4486a09c
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-31 16:33:43 +0200

    ok

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "random_id" "rg_name" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "var.env" {
   name     = "test-${random_id.rg_name.hex}-rg"
   location = var.location
 }
