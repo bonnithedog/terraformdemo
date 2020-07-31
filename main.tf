317e224e9df5ff3633fae2a8c9d3f3dd2a190931
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-31 21:37:42 +0200

    rgname

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 #create resource group
 module "resource_group" "rg" {
     source    = "./modules/rg"
-    rgname    = "demo"
+    #rgname    = "demo"
     location = "westus"
 }
 
